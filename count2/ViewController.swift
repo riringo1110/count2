07004a34ba441ea9ad5b2ce7f114cc23b91325f9
Ririko Nagaishi <riry.cucumber@gmail.com>
2021-02-07 20:36:56 +0900

    Initial Commit

---
+++ count2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  count2
+//
+//  Created by Ririko Nagaishi on 2021/02/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
