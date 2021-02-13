45597a72314cfe1ade9c3cc131411224ef65a264
Ririko Nagaishi <riry.cucumber@gmail.com>
2021-02-13 11:56:50 +0900

    Countアプリ２回目

--- count2/ViewController.swift
+++ count2/ViewController.swift
@@ -21,48 +21,35 @@ class ViewController: UIViewController {
     }
     @IBAction func plus(){
         number = number + 1
-        label.text = String(number)
-        if number >= 10{
-            label.textColor = UIColor.red
-        }else{
-            label.textColor = UIColor.black
-        }
+        textLabel()
     }
 
     @IBAction func minus(){
         number = number - 1
-        label.text = String(number)
-        if number >= 10{
-            label.textColor = UIColor.red
-        }else{
-            label.textColor = UIColor.black
-    }
+        textLabel()
     }
     
     @IBAction func reset(){
         number = 0
-        label.text = String(number)
-        label.textColor = UIColor.black
+        textLabel()
     }
     
     @IBAction func kakeru(){
         number = number*2
-        label.text = String(number)
-        if number >= 10{
-            label.textColor = UIColor.red
-        }else{
-            label.textColor = UIColor.black
-    }
+        textLabel()
     }
     
     @IBAction func waru(){
         number = number/2
+        textLabel()
+}
+    func textLabel() {
         label.text = String(number)
         if number >= 10{
             label.textColor = UIColor.red
         }else{
             label.textColor = UIColor.black
     }
-}
+    }
 }
 
